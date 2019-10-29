633e9467033b3d68bc233db44ee9939cd2aad1eb
tunefish <tunefish@users.noreply.github.com>
2019-10-28 20:55:00 -0400

    Add module code for logging HTTP(S) requests

---
+++ app/src/main/java/tunefish/requestlogger/RealCallHook.java
@@ -0,0 +1,54 @@
+package tunefish.requestlogger;
+
+import android.app.AndroidAppHelper;
+import android.app.Application;
+import android.content.Context;
+
+import java.io.FileOutputStream;
+import java.sql.Timestamp;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+
+
+public class RealCallHook extends XC_MethodHook {
+    @Override
+    protected void beforeHookedMethod(MethodHookParam param) {
+        // Called whenever a okhttp3.Request object has been built and was
+        // scheduled to be executed (but has not been issued yet)
+        try {
+            // Get the okhttp3.Request object form the RealCall object
+            Object request = XposedHelpers.callMethod(param.thisObject, "request");
+            if (request == null) {
+                XposedBridge.log("Got call but REQUEST is null");
+                return;
+            }
+
+            // Get the okhttp3.HttpUrl object from the Request
+            Object url = XposedHelpers.getObjectField(request, "url");
+
+            if (url == null) {
+                XposedBridge.log("Got call but URL is null");
+                return;
+            }
+            XposedBridge.log("REQUEST: " + url);
+
+            Application app = AndroidAppHelper.currentApplication();
+            if (app == null) {
+                XposedBridge.log("Cannot get application context!");
+                return;
+            }
+
+            // Append to "_requests.log" in internal storage of the hooked app
+            FileOutputStream fos = app.openFileOutput("_requests.log", Context.MODE_APPEND);
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            fos.write(String.format("%-31s %s\n", timestamp, url).getBytes());
+            fos.close();
+        } catch (Exception e) {
+            // Log error and don't crash
+            XposedBridge.log("okhttp3 request could not be identified:");
+            XposedBridge.log(e);
+        }
+    }
+}
--- app/src/main/java/tunefish/requestlogger/RequestLogger.java
+++ app/src/main/java/tunefish/requestlogger/RequestLogger.java
@@ -1,12 +1,26 @@
 package tunefish.requestlogger;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 
 public class RequestLogger implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) {
-        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        Class realCall = XposedHelpers.findClassIfExists("okhttp3.RealCall", lpparam.classLoader);
+        if (realCall != null) {
+            // Reuse hook because we're hooking multiple methods of the same class
+            XC_MethodHook hook = new RealCallHook();
+
+            // Call.execute() is used to issue a synchronous request
+            XposedHelpers.findAndHookMethod(realCall,"execute", hook);
+
+            // Call.enqueue(Callback c) is used to issue an asynchronous request
+            XposedHelpers.findAndHookMethod(realCall, "enqueue","okhttp3.Callback", hook);
+
+            XposedBridge.log("Hooked app: " + lpparam.packageName + " with okhttp3");
+        }
     }
 }
